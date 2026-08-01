906b553bf659a1a271341ad0aa73da82af3d2670
Clayver Viegas <clayversousaviegas@gmail.com>
2026-08-01 13:50:08 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Estratégia: cache-first para os arquivos do app, com atualização em segundo plano.
    Ao publicar uma nova versão do index.html, troque o número do CACHE abaixo. */
 
-const CACHE = 'agenda-v8';
+const CACHE = 'agenda-v9';
 const ARQUIVOS = [
   './',
   './index.html',
